9b0a3f12c8682612969f0ac21ad4606e6300b488
che <che.blancoo58@gmail.com>
2021-04-09 23:20:59 +0100

    Add the following Update config file Add gameOptions file Add assets images

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,8 +1,28 @@
 import 'phaser';
-
+/* global Phaser */
+import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin'
 export default {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600
-};
+    width: 800,
+    height: 600,
+        // scene: playGame,
+        backgroundColor:  ' 0x0c88c7',
+ 
+        // physics settings
+        physics: {
+            default: "arcade"
+        },
+        parent: 'phaser-container',
+        dom: {
+          createContainer: true
+      },
+    plugins: {
+      scene: [
+        {
+          key: 'rexUI',
+          plugin: RexUIPlugin,
+          mapping: 'rexUI'
+        }
+      ]
+      }
+    }
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -8,9 +8,57 @@ export default class GameScene extends Phaser.Scene {
   preload () {
     // load images
     this.load.image('logo', 'assets/logo.png');
+    this.add.image(400, 300, 'logo');
+      this.load.image("platform", "assets/ui/cozy/transparent /Full-background.png");
+
+      // player is a sprite sheet made by 24x48 pixels
+      this.load.spritesheet("player", "assets/runner1.png", {
+          frameWidth: 24,
+          frameHeight: 48
+      });
+
+      // the coin is a sprite sheet made by 20x20 pixels
+      this.load.spritesheet("cactus", "cactus.png", {
+          frameWidth: 20,
+          frameHeight: 20
+      });
+
+      // the firecamp is a sprite sheet made by 32x58 pixels
+      // this.load.spritesheet("fire", "fire.png", {
+      //     frameWidth: 40,
+      //     frameHeight: 70
+      // });
+
+      // mountains are a sprite sheet made by 512x512 pixels
+      // this.load.spritesheet("mountain", "mountain.png", {
+      //     frameWidth: 512,
+      //     frameHeight: 512
+      // });
   }
 
   create () {
-    this.add.image(400, 300, 'logo');
+      this.add.image(game.config.with/2,game.config.height/2)
+        // setting player animation
+        this.anims.create({
+          key: "run",
+          frames: this.anims.generateFrameNumbers("player", {
+              start: 0,
+              end: 1
+          }),
+          frameRate: 8,
+          repeat: -1
+      });
+         // setting coin animation
+         this.anims.create({
+          key: "rotate",
+          frames: this.anims.generateFrameNumbers("cactus", {
+              start: 0,
+              end: 5
+          }),
+          frameRate: 15,
+          yoyo: true,
+          repeat: -1
+      });
+      
   }
 };
